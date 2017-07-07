bc583266658529217a2ec0187d27d811c67b4f45
Fahmi Akbar Wildana <fahmi.akbar.w@mail.ugm.ac.id>
2017-07-07 10:03:51 +0700

    patch(init): rename page and file

--- src/pages/graph/graph.ts
+++ src/pages/graph/graph.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-graph',
+  templateUrl: 'graph.html'
 })
-export class HomePage {
+export class GraphPage {
 
   constructor(public navCtrl: NavController) {
 
