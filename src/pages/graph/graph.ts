0ad0657d45194e00a663f3ded3a61fe58b29d7cf
Fahmi Akbar Wildana <fahmi.akbar.w@mail.ugm.ac.id>
2017-07-10 09:30:36 +0700

    feat(d3): Add d3js

--- src/pages/graph/graph.ts
+++ src/pages/graph/graph.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as d3 from "d3";
 
 @Component({
   selector: 'page-graph',
   templateUrl: 'graph.html'
 })
 export class GraphPage {
+  @ViewChild('graph') graphContainer
+
+  ionViewDidLoad() {
+    this.initD3()
+  }
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  initD3() {
+    var chart = d3.select(this.graphContainer.nativeElement)
+      .append("svg")
+      .attr("width", 100)
+      .attr("height", 100)
+      .append("g");
+
+    var rows = [
+      { x: 1, y: 1 },
+      { x: 2, y: 2 },
+      { x: 3, y: 3 },
+    ];
+
+    var xScale = d3.scaleLinear()
+      .range([0, 100])
+      .domain([1, 3]);
+    var yScale = d3.scaleLinear()
+      .range([100, 0])
+      .domain([1, 3]);
+
+    chart.selectAll(".dot")
+      .data(rows)
+      .enter().append("circle")
+      .attr("class", "dot")
+      .attr("cx", (row) => { return xScale(row.x) })
+      .attr("cy", (row) => { return yScale(row.y) })
+      .attr("r", 3.5);
   }
 
 }
